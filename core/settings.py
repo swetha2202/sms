5ae562f887dfd042acc1583495a32c42c9230547
swetha2202 <146186341+swetha2202@users.noreply.github.com>
2026-05-11 14:32:11 +0530

    Set ALLOWED_HOSTS to wildcard

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-key-change-in-production-xyz123')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'edumanage-p1l1.onrender.com').split(',')
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://edumanage-p1l1.onrender.com']
 
 INSTALLED_APPS = [
